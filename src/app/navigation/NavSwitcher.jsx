63f0eb86061550dbfc7b90f2c7408c0b3d2b84ce
amkayondo <amkayondo01@gmail.com>
2020-04-30 20:18:32 +0300

    APP: UPDATE APP

--- src/app/navigation/NavSwitcher.jsx
+++ src/app/navigation/NavSwitcher.jsx
@@ -13,7 +13,8 @@ const NavSwitcher = () => {
   return (
     <div>
       { localStorage.setItem("tbAuthtkn", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyMDIxNDY5MjQiLCJpc0FkbWluIjpmYWxzZSwicGhvbmVOdW1iZXIiOjc4MTI5NTQwNiwiaWF0IjoxNTg4MTkyMjQyLCJleHAiOjE1ODg3OTcwNDJ9.NTqzntf-QEGNqbVbno58Sm46RzZynwIHug5FaWFql7M") }
-      { token === null ? <IndexNav /> : <HomeNav /> }
+      {/* { token === null ? <IndexNav /> : <HomeNav /> } */}
+      <HomeNav />
     </div>
   );
 };
